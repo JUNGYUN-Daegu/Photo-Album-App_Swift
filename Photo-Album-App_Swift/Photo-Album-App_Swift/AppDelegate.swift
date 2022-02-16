fe8e52369c7cea6a31ead8ceff2352b474f6d374
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2022-02-16 09:49:47 +0900

    Create Initial Project

---
+++ Photo-Album-App_Swift/Photo-Album-App_Swift/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Photo-Album-App_Swift
+//
+//  Created by 조중윤 on 2022/02/15.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Photo-Album-App_Swift/Photo-Album-App_Swift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Photo-Album-App_Swift
+//
+//  Created by 조중윤 on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
